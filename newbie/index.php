85036ca382f25f42502b161d0b85dae3102a50fe
Helen Pham <helen.pham@sitehost.co.nz>
2024-05-16 11:14:10 +1200

    release

--- newbie/index.php
+++ newbie/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Releaseplease</title>
+        <title>Helen</title>
     </head>
     <body>
         <p>Running release command againnnnnn....!</p>
