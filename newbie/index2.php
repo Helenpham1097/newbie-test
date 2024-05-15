b8db82f4615ed5158f47e7cf12d6da85f889f359
Helen Pham <helen.pham@sitehost.co.nz>
2024-05-15 16:33:56 +1200

    test release 3

--- newbie/index2.php
+++ newbie/index2.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Demomeme</title>
+        <title>Demomeme2</title>
     </head>
     <body>
-        <p>Hello Demomeme</p>
+        <p>Hello Demomeme 2</p>
     </body>
 </html>
